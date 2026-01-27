735ca5230da0dabd2f14e5292bc7ac9792605aa0
tlam15282@gmail.com <tlam15282@gmail.com>
2026-01-27 09:24:02 +0700

    Config BE

---
+++ sports4u-backend/src/main/java/com/sports4u/sports4u_backend/Sports4uBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sports4u.sports4u_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sports4uBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sports4uBackendApplication.class, args);
+	}
+
+}
---
+++ sports4u-backend/src/test/java/com/sports4u/sports4u_backend/Sports4uBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sports4u.sports4u_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sports4uBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
